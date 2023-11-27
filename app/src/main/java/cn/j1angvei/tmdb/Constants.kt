933a9319f372f28cb3f514377d385865f8a836cb
j1angvei <mjiangwei@outlook.com>
2023-11-27 16:30:36 +0800

    update expired access token

--- app/src/main/java/cn/j1angvei/tmdb/Constants.kt
+++ app/src/main/java/cn/j1angvei/tmdb/Constants.kt
@@ -6,7 +6,7 @@ const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
 const val TMDB_IMG_URL = "https://image.tmdb.org/t/p/original"
 const val TMDB_PAGE_SIZE = 20
 const val TMDB_ACCESS_TOKEN =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkY2MxODY3MmJlYjI4ZGQ1OTYwODU4YzVjM2UzYjE3NCIsInN1YiI6IjU5MWMwZDA0YzNhMzY4N2E4ZTAwNzBiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nqvUK6XGdPta-JFSNJeuAGGoCW0H9Pa1lhIuAFpA8uQ"
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDJlNzI1ZGM5YzI4Mjk2ODA5NGI1ODNjYTU3NDdiZiIsInN1YiI6IjU5MWMwZDA0YzNhMzY4N2E4ZTAwNzBiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HwzP37wXAwYCcefkieGfPHLC8lr5ldagVqHoFQevMpg"
 
 const val DB_CACHE_EXPIRE_DURATION = 1000 * 60 * 10L // 10分钟过期
 
